ef027776342b8a4da5bc4941e983efb467bfc835
Ankit Jain <ankit.jain@xamarin.com>
2016-09-21 12:53:44 -0400

    dump init-tools.log

--- init-tools.sh
+++ init-tools.sh
@@ -77,8 +77,8 @@ if [ ! -e $__PROJECT_JSON_FILE ]; then
     if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then
         echo "Restoring BuildTools version $__BUILD_TOOLS_PACKAGE_VERSION..."
         echo "Running: $__DOTNET_CMD restore \"$__PROJECT_JSON_FILE\" --packages $__PACKAGES_DIR -v Debug --source $__BUILDTOOLS_SOURCE" >> $__init_tools_log
-        $__DOTNET_CMD restore "$__PROJECT_JSON_FILE" --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE >> $__init_tools_log
-        if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then echo "ERROR: Could not restore build tools correctly. See '$__init_tools_log' for more details."; rm "$__PROJECT_JSON_FILE"; exit 1; fi
+        $__DOTNET_CMD restore "$__PROJECT_JSON_FILE" --packages $__PACKAGES_DIR -v Debug --source $__BUILDTOOLS_SOURCE >> $__init_tools_log
+        if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then echo "ERROR: Could not restore build tools correctly. See '$__init_tools_log' for more details."; cat $__init_tools_log ; rm "$__PROJECT_JSON_FILE"; exit 1; fi
     fi
 
     echo "Initializing BuildTools..."
